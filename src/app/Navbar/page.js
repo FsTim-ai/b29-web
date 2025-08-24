beac21d848dafd46d6a0a1794ba637d1039fa3eb
FsTim-ai <timothyfebyant3@gmail.com>
2025-08-24 19:42:49 +0700

    menambah layout, install prisma, zod, react-icons,bcrypt,next-auth, dan prisma-adapter

--- src/app/Navbar/page.js
+++ src/app/Navbar/page.js
@@ -1,22 +1,24 @@
-import Link from "next/link"
-import Mode from "../Mode/page"
+import Link from "next/link";
+import Mode from "../Mode/page";
 
 export default function Navbar() {
-    return(
-        <div className="flex justify-between items-center p-4">
-            <div className="text-xl font-bold">
-            <Link href="/">Home</Link>
-            </div>
-            <div className="text-xl font-bold space-x-5">
-            <Link href="/Mamber">Mamber</Link>
-            <Link href="/Event">Event</Link>
-            <Link href="/Forum">Forum</Link>
-            <Link href="/Study">Study</Link>
-            <Link href="/Galery">Galery</Link>
-            </div>
-            <div>
-            <Mode/>
-            </div>
+  return (
+    <div className="fixed top-0 left-0 w-full">
+      <nav className="flex justify-between items-center p-4">
+        <div className="text-xl font-bold">
+          <Link href="/">Home</Link>
         </div>
-    )
+        <div className="text-xl font-bold space-x-5">
+          <Link href="/Mamber">Mamber</Link>
+          <Link href="/Event">Event</Link>
+          <Link href="/Forum">Forum</Link>
+          <Link href="/Study">Study</Link>
+          <Link href="/Galery">Galery</Link>
+        </div>
+        <div>
+          <Mode />
+        </div>
+      </nav>
+    </div>
+  );
 }
